da1548877d35a320e1029588eb13cfa1b90766c7
abhiyanShrestha-fullstackdeveloper <abhiyanshrestha328@gmail.com>
2024-08-23 11:59:24 +0545

    made connection with frontend and bakend todo work
    table shows backend data in the front end of the index.html with is homepage

--- todowebapp/base/views.py
+++ todowebapp/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Todo
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    todo_objs = Todo.objects.all()
+    data = {'todos': todo_objs}
+    return render(request,'index.html',context=data)
